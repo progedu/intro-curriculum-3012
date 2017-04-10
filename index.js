aac53cd4357eae85cdc2c08d56f117d8c5f24a54
DDRAGON <davide.0829@gmail.com>
2017-04-10 17:07:29 +0900

    Content-Type の修正

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 	console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
 	res.writeHead(200, {
-		'Content-Type': 'text/plain',
-		'charset': 'utf-8'
+		'Content-Type': 'text/plain; charset=utf-8'
 	});
 	res.write(req.headers['user-agent']);
 	res.end();
