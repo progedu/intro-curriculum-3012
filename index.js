fc81afdab20afde9640c296337959f6a22b69cb5
tobihaze <tobihaze_2017@yahoo.co.jp>
2018-10-01 11:15:17 +0000

    日付の位置を変更し、new Date()を変数に入れる

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict';
 const http = require('http');
+const d = new Date()
 const server = http.createServer((req, res) => {
-	console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
+	console.info('[' + d + '] Requested by ' + req.connection.remoteAddress);
 	res.writeHead(200, {
 		'Content-Type': 'text/plain; charset=utf-8'
 	});
 	res.write(req.headers['user-agent']);
 	res.end();
 }).on('error', (e) => {
-	console.error('[' + new Date() + '] Server Error', e);
+	console.error('[' + d + '] Server Error', e);
 }).on('clientError', (e) => {
-	console.error('[' + new Date() + '] Client Error', e);
+	console.error('[' + d + '] Client Error', e);
 });
 const port = 8000;
 server.listen(port, () => {
-	console.info('Listening on ' + port + ' [' + new Date() + ']');
+	console.info('[' + d + '] Listening on ' + port);
 });
