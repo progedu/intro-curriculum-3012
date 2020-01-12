f82558e2134b9d32aad9e685fc683a0271e22f7b
p-t-a-p-1 <kb240225@gmail.com>
2020-01-12 23:59:45 +0900

    [fix] サーバー起動時のconsole.logをconsole.infoに修正（日付も追加）

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-'use strict';
-const http = require('http');
-const server = http.createServer((req, res) => {
-	console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
-	res.writeHead(200, {
-		'Content-Type': 'text/plain; charset=utf-8'
-	});
-	res.write(req.headers['user-agent']);
-	res.end();
-}).on('error', (e) => {
-	console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
-	console.error('[' + new Date() + '] Client Error', e);
-});
-const port = 8000;
+'use strict'
+const http = require('http')
+const server = http
+  .createServer((req, res) => {
+    // 情報を出力したいという意図を明確にするためにlogではなくinfoを使う
+    // req.connection.remoteAddress でリクエストが送られたIPアドレス
+    console.info(
+      '[' + new Date() + '] Requested By ' + req.connection.remoteAddress
+    )
+    res.writeHead(200, {
+      'Content-Type': 'text/plain; charset=utf-8'
+    })
+    res.write(req.headers['user-agent'])
+    res.end()
+  })
+  .on('error', e => {
+    // 'error'という文字列のイベントが発生した時はサーバーエラーとしてエラーログ出力
+    console.error('[' + new Date() + '] Server Error', e)
+  })
+  .on('clientError', e => {
+    // 'client error'という文字列のイベントが発生した時はクライアントエラーとしてエラーログ出力
+    console.error('[' + new Date() + '] Client Error', e)
+  })
+const port = 8000
 server.listen(port, () => {
-	console.log('Listening on ' + port);
-});
+  console.info('[' + new Date() + '] Listening on ' + port)
+})
