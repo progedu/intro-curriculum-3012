d5581595a9bf2fce75cfdf91963ae36852b57bbb
techfore <beforethetech@gmail.com>
2020-04-05 19:10:15 +0900

    add log event

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-	console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
+	// console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
 	res.writeHead(200, {
 		'Content-Type': 'text/plain; charset=utf-8'
 	});
@@ -14,5 +14,5 @@ const server = http.createServer((req, res) => {
 });
 const port = 8000;
 server.listen(port, () => {
-	console.log('Listening on ' + port);
+	console.info('[' + new Date() + '] Server Info' + port);
 });
