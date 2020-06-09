406736bee22c8858874d388bbe544e0df108a327
miyatoriOIL <miyatorioil@gmail.com>
2020-06-09 23:19:08 +0900

    log.infoの追記

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-	console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
-	res.writeHead(200, {
-		'Content-Type': 'text/plain; charset=utf-8'
-	});
-	res.write(req.headers['user-agent']);
-	res.end();
+    console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=utf-8'
+    });
+    res.write(req.headers['user-agent']);
+    res.end();
 }).on('error', (e) => {
-	console.error('[' + new Date() + '] Server Error', e);
+    console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
-	console.error('[' + new Date() + '] Client Error', e);
+    console.error('[' + new Date() + '] Client Error', e);
 });
 const port = 8000;
 server.listen(port, () => {
-	console.log('Listening on ' + port);
+    console.log('[' + new Date() + '] Listening on ' + port);
 });
