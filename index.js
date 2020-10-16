99f3ae9732b776261b1539d233be41dcc276b2a1
heavyfox <steamtrader@gmai.com>
2020-10-16 20:08:01 +0900

    バッククォーテーションに一部変更

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-	console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
+	console.info(`[${new Date()}] Requested by ${req.connection.remoteAddress}`);
 	res.writeHead(200, {
 		'Content-Type': 'text/plain; charset=utf-8'
 	});
