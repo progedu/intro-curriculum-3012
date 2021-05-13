1fcbc5d7b210d67808bbe6c8e06b454f50d4dee2
Kengo Yagishita <yagishitakengo1002@gmail.com>
2021-05-13 22:11:58 +0900

    起動時ログをinfo関数で出力するよう修正

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ const server = http.createServer((req, res) => {
 });
 const port = 8000;
 server.listen(port, () => {
-	console.log('Listening on ' + port);
+	// ログをinfo関数で出力するよう修正
+	console.info('[' + new Date() + '] Listening on ' + port);
+	//console.log('Listening on ' + port);
 });
